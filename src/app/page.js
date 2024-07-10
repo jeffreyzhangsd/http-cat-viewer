916c33c57df2f7100bd2453a8845d7060e2d67f8
Jeffrey Zhang <109628105+JeffreyZhangsd@users.noreply.github.com>
2024-07-09 18:37:53 -0700

    browser icon update'''''

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import MDXContent from "../components/MDXContent";
+import next_icon from "../../public/next.svg";
 
 // Home page that website first directs to
 // Able to edit with markdown at app/mdx/HomeContent.mdx
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-full max-h-screen p-6">
+      <link rel="icon" href={next_icon} type="image/svg+xml" sizes="any" />
       <MDXContent />
     </main>
   );
